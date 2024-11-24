7256ba77349c7de2f0aee31105f1605d7db230b0
fans656 <fans656@yahoo.com>
2024-11-24 20:42:44 +0800

    add doy port

--- src/fans/ports.py
+++ src/fans/ports.py
@@ -1,9 +1,12 @@
 class ports:
 
-    fme = 6561
-    quantix = 6562
-    auth = 6563
-    stome = 6565
-    stome_desktop = 6660
+    auth            = 6560
+    fme             = 6561
+    quantix         = 6562
+    doy             = 6563
 
-    fansjs_dev = 16501
+    stome           = 6565
+
+    stome_desktop   = 6660
+
+    fansjs_dev      = 16501
